c05a14797a1149d9ccc474722a97b46f719f45f2
Harish Babu <harish_babu@outlook.com>
2019-02-06 20:14:22 +0000

    google analytics tracking id added

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-56557611-1`
       }
     },
     `gatsby-plugin-feed`,
